b417f1e24a73c6343e81560d81ec5015f0410206
catch-up-me <catchupmessenger@gmail.com>
2025-10-30 14:53:26 +0100

    Create Story.js

---
+++ Story.js
@@ -0,0 +1,9 @@
+function Story() {
+  return (
+    <div className="flex items-center justify-center py-20 px-4">
+      <p className="text-gray-400 text-center">
+        When your friends post stories, they'll appear here
+      </p>
+    </div>
+  );
+}
